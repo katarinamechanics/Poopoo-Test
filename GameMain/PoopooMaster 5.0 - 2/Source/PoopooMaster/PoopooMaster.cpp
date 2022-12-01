09e37eca9c6cf2d43b45e7308b025b2262b0deda
katarinamechanics <mattbrodowski1@yahoo.com>
2022-12-01 18:23:54 -0500

    mechanic update
    update to 5.0.3
    test map made
    base grass mechanic
    basic state switcher from overworld to battle

--- GameMain/PoopooMaster 5.0 - 2/Source/PoopooMaster/PoopooMaster.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "PoopooMaster.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PoopooMaster, "PoopooMaster" );
--- GameMain/PoopooMaster/Intermediate/Build/Win64/PoopooMasterEditor/Development/Engine/SharedPCH.Engine.cpp.old
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.30.30706
-#include "C:/Users/mattb/Documents/GitHub/Poopoo-Test/GameMain/PoopooMaster/Intermediate/Build/Win64/PoopooMasterEditor/Development/Engine/SharedPCH.Engine.h"
